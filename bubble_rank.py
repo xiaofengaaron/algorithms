6ba9e4c2cd30b2945196129c5211c86f016962b0
xiaofengaaron <xiaofeng.lang@hotmail.com>
2020-09-27 20:44:40 +1000

    full

--- bubble_rank.py
+++ bubble_rank.py
@@ -29,6 +29,62 @@ def insertion_sort(arr):
 		arr[pre_index+1] = current
 	return arr
 
+# merge sort
+def merge_sort(arr):
+	# 递归将arr分成两个一组
+	n = len(arr)
+	if n <= 1: return arr
+	mid = n // 2
+	left = arr[0:mid]
+	right = arr[mid:]
+	return merge(merge_sort(left), merge_sort(right))
+
+def merge(left, right):
+	l, r, left_len, right_len = 0, 0, len(left), len(right)
+
+	result = []
+	while l < left_len and r < right_len:
+		if left[l] < right[r]:
+			result.append(left[l])
+			l += 1
+		else:
+			result.append(right[r])
+			r += 1
+	while l < left_len:
+		result.append(left[l])
+		l += 1
+	while r < right_len:
+		result.append(right[r])
+		r += 1
+	return result
+
+
+# quick sort
+def quick_sort(arr, low, high):
+	if len(arr) == 1: return arr
+	# partition--sorting
+	if low < high:
+		pivot_index = partition(arr, low, high)
+
+		# recursion for each partition
+		quick_sort(arr, low, pivot_index-1)
+		quick_sort(arr, pivot_index+1, high)
+	return arr
+
+def partition(arr, low, high):
+	# pointer to swap with pivot
+	i = low-1
+	# pivot
+	pivot_value = arr[high]
+	for j in range(low, high):
+		if arr[j] < pivot_value:
+			i += 1
+			arr[i], arr[j] = arr[j] , arr[i]
+	arr[i+1], arr[high] = arr[high], arr[i+1]
+	return i+1
+print(quick_sort([43,2,2,3,4,5,1,1,3], 0, 8)) 
+
+
 
 
 
